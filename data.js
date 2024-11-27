0cb7bac38a45b084a42e57042b96e3de53ff7ddb
IkuroMaira <gwen.bussac@gmail.com>
2024-11-27 16:59:01 +0100

    add display 3 pokemons and range

--- data.js
+++ data.js
@@ -23,8 +23,6 @@ async function pokemonAPI() {
     displayElement.appendChild(divImage);
 }
 
-// pokemonAPI();
-
 // Afficher les pokémons par le type
 async function displayPokemonsByType(type) {
     const resp = await fetch("https://tyradex.vercel.app/api/v1/gen/1");
@@ -42,18 +40,6 @@ async function displayPokemonsByType(type) {
     // });
 
     return filteredPokemons;
-}
-
-async function displayThreeRandomPokemons(type) {
-
-    const filteredPokemons = await displayPokemonsByType(type);
-    // console.log(filteredPokemons);
-    const shuffledPokemons = shuffle(filteredPokemons);
-    console.log(shuffledPokemons);
-
-    // Récupérer que 3 pokémons
-    // const threeRandomPokemons = shuffledPokemons.slice(0, 3);
-    // console.log(threeRandomPokemons);
 }
 
 // Fonction pour mélanger le tableau (algorithme de Fisher-Yates)
