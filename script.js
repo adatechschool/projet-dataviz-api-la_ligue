967fdecd2159bcecff47f978299343cf58855e21
AssiarDev <halibaraissa@gmail.com>
2024-11-27 16:41:56 +0100

    ajout des fonctions getRandomPokemons & displayRandomPkmn

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const fetchPokemonData = async (pkmnType) => {
         const response = await fetch('https://tyradex.vercel.app/api/v1/gen/1')
         const data = await response.json();
 
+        const matchingPokemons = [];
+
         for(const pokemon of data) {
             if (pokemon.types.find((type) => {
                 return type.name.toLowerCase() === pkmnType
             }))
-            console.log('POKEMON:', pokemon.name.fr)
+            matchingPokemons.push(pokemon.name.fr)
+            //console.log('POKEMON:', pokemon.name.fr)
         }
+
+        return matchingPokemons;
 
     } catch (error) {
         console.error('Erreur lors de la récupération des données:', error)
@@ -16,4 +21,16 @@ const fetchPokemonData = async (pkmnType) => {
     }
 }
 
-fetchPokemonData('plante')
+const getRandomPokemons = (pokemons, count) => {
+    const shuffled = pokemons.sort(() => 0.5 - Math.random())
+    return shuffled.slice(0, count)
+}
+
+
+const displayRandomPkmn = async (type) => {
+    const allPokemons = await fetchPokemonData(type);
+    const randomPokemons = getRandomPokemons(allPokemons, 3)
+    return randomPokemons
+}
+
+console.log(displayRandomPkmn('feu'))
