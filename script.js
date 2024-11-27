95e34f07fd41ff2616d6f818945673c25d4f2105
AssiarDev <halibaraissa@gmail.com>
2024-11-27 15:50:18 +0100

    correction nom du paramètre de la fonction

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const fetchPokemonData = async (userType) => {
+const fetchPokemonData = async (pkmnType) => {
     try {
         const response = await fetch('https://tyradex.vercel.app/api/v1/gen/1')
         const data = await response.json();
 
         for(const pokemon of data) {
             if (pokemon.types.find((type) => {
-                return type.name.toLowerCase() === userType
+                return type.name.toLowerCase() === pkmnType
             }))
             console.log('POKEMON:', pokemon.name.fr)
         }
@@ -15,3 +15,5 @@ const fetchPokemonData = async (userType) => {
         return []
     }
 }
+
+fetchPokemonData('plante')
