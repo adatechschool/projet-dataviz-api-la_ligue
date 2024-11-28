dff3237f616d4a8580a815de2b90db0252c936e0
AssiarDev <halibaraissa@gmail.com>
2024-11-28 10:24:15 +0100

    update

--- script.js
+++ script.js
@@ -5,21 +5,22 @@ const fetchPokemonData = async (pkmnType) => {
 
         const matchingPokemons = [];
 
+        //Obtenir les types de pkmns en fonction du choix de l'utilisateur
         for(const pokemon of data) {
             if (pokemon.types.find((type) => {
                 return type.name.toLowerCase() === pkmnType
             }))
             matchingPokemons.push(pokemon.name.fr)
             //console.log('POKEMON:', pokemon.name.fr)
-        }
+        };
 
         return matchingPokemons;
 
     } catch (error) {
         console.error('Erreur lors de la récupération des données:', error)
         return []
-    }
-}
+    };
+};
 
 const getRandomPokemons = (pokemons, count) => {
     const shuffled = pokemons.sort(() => 0.5 - Math.random())
@@ -29,8 +30,13 @@ const getRandomPokemons = (pokemons, count) => {
 
 const displayRandomPkmn = async (type) => {
     const allPokemons = await fetchPokemonData(type);
-    const randomPokemons = getRandomPokemons(allPokemons, 3)
-    return randomPokemons
-}
+    const randomPokemons = getRandomPokemons(allPokemons, 3);
 
-console.log(displayRandomPkmn('feu'))
+    // Interaction dans le DOM
+    let p = document.createElement('p');
+    p.innerText = randomPokemons;
+    document.querySelector('body').appendChild(p)
+
+    //return randomPokemons
+}
+displayRandomPkmn('feu')
