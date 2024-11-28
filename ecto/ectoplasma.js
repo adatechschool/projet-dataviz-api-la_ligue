8e10a005162bc2bef5e3fcc2ab3e4d677165dd29
AngieaDA <ruggiero.teodosio@gmail.com>
2024-11-28 14:42:30 +0100

    teste

---
+++ ecto/ectoplasma.js
@@ -0,0 +1,38 @@
+//async function loadPokémon(){
+  //  const res = await fetch('https://tyradex.app/api/v1/gen/8');
+    //const data = await res.json();
+   //console.log(data[0])
+    //console.log(data[0].stats.vit)
+    //console.log(data[0].weight)
+    //console.log(data[0].name.jp)
+    //console.log(data[0].resistances[2].name)
+    //console.log(data[0].talents[1].name)
+    //console.log(data[0].resistances[7].name)
+    //console.log(data[0].poisson[7].mu)
+//}
+//loadPokémon()
+
+// il faut afficher le nom du pokémon 
+const ectoplasma = {
+    console.log(data[0].nature)
+    nom: "ectoplasme",
+    numero:93,
+    types: ["spectre"],
+    taille: "1,5 m",
+    poids: "40,5 kg",
+    talent: "Corps Maudit",
+    sexe: "50 % femelle; 50 % mâle",
+    nature: "Pokémon Ombre",
+    statistiques: {
+        pv: 60,
+        attaque: 65,
+        defense: 60,
+        AttaqueSpé: 130,
+        DéfenceSpé: 75,
+        Vitesse: 
+         
+
+    }
+  
+
+//}
